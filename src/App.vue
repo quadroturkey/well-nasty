cb658d4aa061ac5892783c604fbcb40d6615caee
qudroturkey <reesemb1@gmail.com>
2021-09-13 15:02:22 -0500

    add vue files

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Header/>
+  <!-- <HelloWorld msg="Hello Vue 3.0 + Vite" /> -->
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <div class="container">
+    <img class="logo" src="/logo.png">
+    <div class="links">
+      <a class="link" v-for="link in links" :key="link" :href="'/' + link" @click="navigate(link)">{{ link }}</a>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+import '../styles/header.css'
+
+export default {
+  name: 'Header',
+  data() {
+    return {
+      links: ["home", "film", "workout"]
+    }
+  }
+}
+
+</script>
