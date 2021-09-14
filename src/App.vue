d33f9ca6d7169091ae3f284980b26a7330c0d73b
qudroturkey <reesemb1@gmail.com>
2021-09-13 19:48:36 -0500

    spacing

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,38 @@
 <template>
+
   <Header @changePage="changePage"/>
+  
   <Homepage v-show="page == 'home'"/>
   <Film v-show="page == 'film'"/>
   <Workout v-show="page == 'workout'"/>
+
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Homepage from './components/Homepage.vue'
-import Film from './components/Film.vue'
-import Workout from './components/Workout.vue'
 
-export default {
-  name: 'App',
-  components: {
-    Header,
-    Homepage,
-    Film,
-    Workout
-  },
-  data() {
-    return {
-      page: 'home',
-    }
-  },
-  methods: {
-    changePage(data) {
-      this.page = data.link
+  import Header from './components/Header.vue'
+  import Homepage from './components/Homepage.vue'
+  import Film from './components/Film.vue'
+  import Workout from './components/Workout.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      Header,
+      Homepage,
+      Film,
+      Workout
+    },
+    data() {
+      return {
+        page: 'home',
+      }
+    },
+    methods: {
+      changePage(data) {
+        this.page = data.link
+      }
     }
   }
-}
+
 </script>
