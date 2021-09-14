0b854928958974a3960068c980996d0adba50108
qudroturkey <reesemb1@gmail.com>
2021-09-13 19:49:48 -0500

    logo navs to homepage

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
 
   <div class="container">
-    <img class="logo" src="/logo.png">
+    <img class="logo" @click="changePage('home')" src="/logo.png">
     <div class="links">
       <a class="link" v-for="link in links" :key="link" @click="changePage(link)">{{ link }}</a>
     </div>
